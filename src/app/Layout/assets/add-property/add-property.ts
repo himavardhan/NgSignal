d842fa4012ee0a447344c60266475052f56c341b
Himavardhan Reddy Donuru <himavardhan.d@gmail.com>
2026-02-03 09:43:34 -0600

    Added Few new components

---
+++ src/app/Layout/assets/add-property/add-property.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-property',
+  imports: [],
+  templateUrl: './add-property.html',
+  styleUrl: './add-property.scss',
+  standalone: true
+})
+export class AddProperty {
+
+}
--- src/app/Layout/assets/assets.ts
+++ src/app/Layout/assets/assets.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
+import { SharedModule } from '../../SharedModules/SharedModule';
+
 
 @Component({
   selector: 'app-assets',
-  imports: [],
+  imports: [SharedModule],
   templateUrl: './assets.html',
   styleUrl: './assets.scss',
+  standalone: true
 })
 export class Assets {
 
+  addProperty(){
+    console.log('Add Property button clicked');
+  }
+   
 }
