17e67c731b9c346af2550a898192c2b868619465
Himavardhan Reddy Donuru <himavardhan.d@gmail.com>
2026-05-07 19:11:12 -0500

    Added reactive form validation to display them in the template

--- src/app/Components/booking-form/booking-form.ts
+++ src/app/Components/booking-form/booking-form.ts
@@ -1,6 +1,14 @@
 import { Component, inject } from '@angular/core';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
-import { FormsModule , ReactiveFormsModule, FormBuilder, Validators, FormArray} from '@angular/forms';
+import {
+  FormsModule,
+  ReactiveFormsModule,
+  FormBuilder,
+  Validators,
+  FormArray,
+  FormControl,
+} from '@angular/forms';
+import { label } from '@primeuix/themes/aura/metergroup';
 import { debounceTime, distinctUntilChanged } from 'rxjs';
 
 @Component({
@@ -8,81 +16,96 @@ import { debounceTime, distinctUntilChanged } from 'rxjs';
   imports: [FormsModule, ReactiveFormsModule],
   templateUrl: './booking-form.html',
   styleUrl: './booking-form.scss',
-  standalone:true
+  standalone: true,
 })
-
-
 export class BookingForm {
-
-  departure:string = '';
-  Arrival:string = '';
+  departure: string = '';
+  Arrival: string = '';
   fb = inject(FormBuilder);
   bookingForm = this.fb.group({
-    depart:[null,Validators.required],
-    arrival:[null,Validators.required],
-    tripType:['Two Way', Validators.required],
-    departDate:[null,Validators.required],
-    returnDate:[null,[Validators.required]],
-    passengerCount:[null,[Validators.required,Validators.min(1)]],
-    passengerInfo:this.fb.array([])
+    depart: ['', [Validators.required, Validators.minLength(4)]],
+    arrival: [null, Validators.required],
+    tripType: ['Two Way', Validators.required],
+    departDate: [null, Validators.required],
+    returnDate: [null, [Validators.required]],
+    passengerCount: [null, [Validators.required, Validators.min(1)]],
+    passengerInfo: this.fb.array([]),
   });
 
-  constructor(){
-    this.bookingForm.get('passengerCount')?.valueChanges.pipe(
-      debounceTime(400),        // 1. Wait 400ms after last keystroke
-      distinctUntilChanged(),
-      takeUntilDestroyed()
-    ).subscribe((item)=>{
-      this.passengerInfo.clear();
-      if(item && item > 0){
-        
-         console.log(item)
-       for (let index = 0; index < item; index++) {
-         this.addPassenger();
-        
-       }
-      }
-  
-    })
+  // Reactive form Validation.
+  // this.departInfo.dirty;
+  // this.departInfo.pristine;
+  // this.departInfo.valid;
+  // this.departInfo.touched && this.departInfo.dirty
+  // this.departInfo.invalid;
+  // this.departInfo.hasError('required')
 
-  this.bookingForm.get('tripType')?.valueChanges.pipe(
-      debounceTime(400),        // 1. Wait 400ms after last keystroke
-      distinctUntilChanged(),
-      takeUntilDestroyed()
-    ).subscribe((data)=>{
-      
-    if(data == 'One Way'){
-      this.bookingForm.get('returnDate')?.setValue(null);
-        this.bookingForm.get('returnDate')?.disable();
-    }else{
-       this.bookingForm.get('returnDate')?.enable();
-    }
-  
-    })
+  constructor() {
+    this.bookingForm
+      .get('passengerCount')
+      ?.valueChanges.pipe(
+        debounceTime(400), // 1. Wait 400ms after last keystroke
+        distinctUntilChanged(),
+        takeUntilDestroyed(),
+      )
+      .subscribe((item) => {
+        this.passengerInfo.clear();
+        if (item && item > 0) {
+          console.log(item);
+          for (let index = 0; index < item; index++) {
+            this.addPassenger();
+          }
+        }
+      });
+
+    this.bookingForm
+      .get('tripType')
+      ?.valueChanges.pipe(distinctUntilChanged(), takeUntilDestroyed())
+      .subscribe((data) => {
+        if (data == 'One Way') {
+          this.bookingForm.get('returnDate')?.setValue(null);
+          this.bookingForm.get('returnDate')?.disable();
+        } else {
+          this.bookingForm.get('returnDate')?.enable();
+        }
+      });
+
+    this.bookingForm
+      .get('depart')
+      ?.valueChanges.pipe(distinctUntilChanged(), takeUntilDestroyed())
+      .subscribe((data) => {
+        if (data && data?.length >= 3) {
+          this.bookingForm.controls.depart.setValue(data.toUpperCase());
+          console.log('depart =', data);
+        }
+      });
   }
 
-  get passengerInfo(){
+  get passengerInfo() {
     return this.bookingForm.get('passengerInfo') as FormArray;
   }
 
-  private addNewPassenger(){
+  get departInfo() {
+    return this.bookingForm.get('depart') as FormControl;
+  }
+
+  private addNewPassenger() {
     return this.fb.group({
-      name:[null,Validators.required],
-      age:[null, [Validators.required,Validators.max(100)]]
+      name: [null, Validators.required],
+      age: [null, [Validators.required, Validators.max(100)]],
     });
   }
 
-  public addPassenger(){
-     this.passengerInfo.push(this.addNewPassenger());
+  public addPassenger() {
+    this.passengerInfo.push(this.addNewPassenger());
   }
 
-  public submitForm(){
+  public submitForm() {
     console.log(this.departure);
   }
 
-  public onSubmit(){
-    const data = this.bookingForm.value ;
-    console.log( data);
+  public onSubmit() {
+    const data = this.bookingForm.value;
+    console.log(data);
   }
-
 }
